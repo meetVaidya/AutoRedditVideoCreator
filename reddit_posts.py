6ddd9920d1f27a199f64da237101c71b727934a7
meext <vaidyameet2004@gmail.com>
2024-05-07 21:57:43 +0530

    feat: Update reddit_posts.py to retrieve top 11 posts and include upvotes and comments
    This commit modifies the `reddit_posts.py` file to retrieve the top 11 posts from the "AITAH" subreddit instead of the previous limit of 10. Additionally, it includes the number of upvotes and comments for each post in the `content_dict` dictionary. This change enhances the functionality of the application by providing more comprehensive information about the top posts.

--- reddit_posts.py
+++ reddit_posts.py
@@ -1,9 +1,17 @@
 import praw
+import os
 import json
+from dotenv import load_dotenv
 
-reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
-                     client_secret='YOUR_CLIENT_SECRET',
-                     user_agent='your_user_agent')
+load_dotenv()  # take environment variables from .env.
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+user_agent = os.getenv("USER_AGENT")
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent)
 
 # Specify the subreddit name
 subreddit_name = 'AITAH'
@@ -12,15 +20,16 @@ subreddit_name = 'AITAH'
 subreddit = reddit.subreddit(subreddit_name)
 
 # Get the top 10 posts from the subreddit
-top_posts = subreddit.hot(limit=10)
+top_posts = list(subreddit.hot(limit=11))[1:]
 
 content_dict = {}
 
 for post in top_posts:
     content_dict[post.id] = {
         'title': post.title,
-        'selftext': post.selftext
+        'selftext': post.selftext,
+        'upvotes': post.score,
+        'comments' : post.num_comments,
     }
+# print(json.dumps(content_dict, indent=4))
 
-with open('output.json', 'w') as f:
-    json.dump(content_dict, f)
